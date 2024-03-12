e7119fbfa53513ef71f8d00f0ea958a8fb0425be
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2024-03-12 15:06:21 +0530

    index js file updated.

--- index.js
+++ index.js
@@ -2,70 +2,100 @@ const drop1 = document.getElementById('drop-1')
 const drop2 = document.getElementById('drop-2')
 const drop3 = document.getElementById('drop-3')
 const drop4 = document.getElementById('drop-4')
-const dropdown1 = document.getElementById('dropdown-1')
-const dropdown2 = document.getElementById('dropdown-2')
-const dropdown3 = document.getElementById('dropdown-3')
-const dropdown4 = document.getElementById('dropdown-4')
+
+const loans = document.getElementById('loans');
+const insurance = document.getElementById('insurance');
+const creditCards = document.getElementById('credit-cards');
+const resources = document.getElementById('resources');
+
+const personal = document.getElementById('personal')
+const business = document.getElementById('business')
+const instant = document.getElementById('instant')
+const professional = document.getElementById('professional')
+const secure = document.getElementById('secure')
+
+const personalLoan = document.getElementById('personalLoan')
+const businessLoan = document.getElementById('businessLoan')
+const instantLoan = document.getElementById('instantLoan')
+const professionalLoan = document.getElementById('professionalLoan')
+const secureLoan = document.getElementById('secureLoan')
 
 
 drop1.addEventListener('click', function(e){
   e.preventDefault()
-  
-  // const dropdown1 = document.getElementById('dropdown-1')
-  dropdown1.innerHTML = `
-  <div>
-    <div>Personal Loan</div>
-    <div>Business Loan</div>
-    <div>Instant Loan</div>
-    <div>Professional Loan</div>
-    <div>Secure Loan</div>
-  </div>`
-  dropdown2.innerHTML = ``
-  dropdown3.innerHTML = ``
-  dropdown4.innerHTML = ``
+  Dropdown1(loans)
 })
 
 drop2.addEventListener('click', function(e){
   e.preventDefault()
-  
-  // const dropdown2 = document.getElementById('dropdown-2')
-  dropdown2.innerHTML = `
-  <div>
-    <div>Health Insurance</div>
-    <div>Life Insurance</div>
-    <div>Term Insurance</div>
-  </div>`
-  dropdown1.innerHTML = ``
-  dropdown3.innerHTML = ``
-  dropdown4.innerHTML = ``
+  Dropdown1(insurance)
 })
 
 drop3.addEventListener('click', function(e){
   e.preventDefault()
-  
-  // const dropdown3 = document.getElementById('dropdown-3')
-  dropdown3.innerHTML = `
-  <div>
-    <div>HDFC Bank Credit Card</div>
-    <div>IDFC Bank Credit Card</div>
-  </div>`
-  dropdown1.innerHTML = ``
-  dropdown2.innerHTML = ``
-  dropdown4.innerHTML = ``
+  Dropdown1(creditCards)
 })
 
 drop4.addEventListener('click', function(e){
   e.preventDefault()
-  
-  // const dropdown4 = document.getElementById('dropdown-4')
-  dropdown4.innerHTML = `
-  <div>
-    <div>Gallery</div>
-    <div>Blogs</div>
-    <div>News</div>
-    <div>EMI Calculator</div>
-  </div>`
-  dropdown1.innerHTML = ``
-  dropdown2.innerHTML = ``
-  dropdown3.innerHTML = ``
+  Dropdown1(resources)
 })
+
+personal.addEventListener('click', function(e){
+  e.preventDefault()
+  Dropdown2(personalLoan)
+})
+
+business.addEventListener('click', function(e){
+  e.preventDefault()
+  Dropdown2(businessLoan)
+})
+
+instant.addEventListener('click', function(e){
+  e.preventDefault()
+  Dropdown2(instantLoan)
+})
+
+professional.addEventListener('click', function(e){
+  e.preventDefault()
+  Dropdown2(professionalLoan)
+})
+
+secure.addEventListener('click', function(e){
+  e.preventDefault()
+  Dropdown2(secureLoan)
+})
+
+function Dropdown1(dropdown) {
+  if (dropdown.style.display === 'none'){
+    hideAllDropdowns1();
+    dropdown.style.display='block';
+  }
+  else {
+    dropdown.style.display= 'none';
+  }
+}
+
+function hideAllDropdowns1 (){
+  const allDropdowns = [loans, insurance, creditCards, resources];
+  allDropdowns.forEach(function (dropdown){
+    dropdown.style.display = 'none';
+  })
+}
+
+function Dropdown2(dropdown) {
+  if (dropdown.style.display === 'none'){
+    hideAllDropdowns2();
+    dropdown.style.display='block';
+  }
+  else {
+    dropdown.style.display= 'none';
+  }
+}
+
+function hideAllDropdowns2 (){
+  const allDropdowns = [personalLoan, businessLoan, instantLoan, professionalLoan, secureLoan];
+  allDropdowns.forEach(function (dropdown){
+    dropdown.style.display = 'none';
+  })
+}
